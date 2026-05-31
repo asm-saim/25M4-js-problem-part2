d1ec9e97753c1474497380288a706fe97fefb190
asm-saim <saim777m@gmail.com>
2026-05-31 22:57:57 +0600

    total salary given by a company is calculated

--- tasks.js
+++ tasks.js
@@ -58,12 +58,12 @@ console.log(`Total money required: ${res} tk`);
 // Input 
 function findAveragePhonePrice(mobiles) {
     // console.log(mobiles.length);
-    let sum=0;
+    let sum = 0;
 
-    for(let mobile of mobiles){
-       sum=sum+mobile.price;       
+    for (let mobile of mobiles) {
+        sum = sum + mobile.price;
     }
-    return sum/mobiles.length;
+    return sum / mobiles.length;
 
 }
 const phones = [
@@ -79,14 +79,23 @@ console.log(`Average price of the phone is ${result4.toFixed(2)} tk`);
 
 
 
-
-
 // Task -5: (Hard)
 // For each employee their current salary is calculated by multiplying yearly increment with experience then adding the result to the starting salary. Now calculate is the total salary has to be provided by the company in a month.
 
-//  const employees = [
-//             { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
-//             { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
-//             { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
-//             { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
-//         ];
+function currentSalary(employees) {
+    let total = 0;
+    for (let employee of employees) {
+        let totalSalary = employee.starting + employee.experience * employee.increment;
+        total += totalSalary;
+    }
+    return total;
+}
+const employees = [
+    { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+    { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+    { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+    { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+];
+const salaryTotal = currentSalary(employees);
+console.log(`Total salary provided by the company in a month: ${salaryTotal} tk`);
+
